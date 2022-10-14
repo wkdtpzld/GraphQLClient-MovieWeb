54a55895044ff0d26510dcf851336e5c151d8052
wkdtpzld <87063105+wkdtpzld@users.noreply.github.com>
2022-10-14 17:48:08 +0900

    Apollo Client And CSS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import client from './client';
+import { ApolloProvider } from '@apollo/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+
+    <ApolloProvider client={client}>
         <App />
-    </React.StrictMode>
+    </ApolloProvider>
 );
